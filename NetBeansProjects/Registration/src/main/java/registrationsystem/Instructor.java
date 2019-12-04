97e4a9705ee6f5258f453bf9ac8d07ffe93aff3c
Xolvimqo <last_wish_6066@hotmail.com>
2019-12-04 10:29:00 -0500

    Registration Assignment

--- NetBeansProjects/Registration/src/main/java/registrationsystem/Instructor.java
+++ NetBeansProjects/Registration/src/main/java/registrationsystem/Instructor.java
@@ -11,18 +11,14 @@ import java.util.List;
 
 /**
  *
- * @author Yu-Ming(Terry) Gu
- * @StudentNumber 200410610
- * @Description: Represents a instructor who teaches in the school
+ * @author Last_
  */
 public class Instructor {
-    // variables
     private String firstName, lastName, address, city, zipCode;
     private int instructorId;
     private LocalDate dateOfRegistration, dateOfBirth;
     private List<String> subjectsToTeach = new ArrayList<String>();
     
-    // constructor
     public Instructor(String fName, String lName, int instructorId, String address, String city, String zipCode, LocalDate dOR, LocalDate dOB) {
         firstName = fName;
         lastName = lName;
@@ -32,108 +28,47 @@ public class Instructor {
         this.zipCode = zipCode;
         dateOfRegistration = dOR;
         dateOfBirth = dOB;
-        
-        // throw error if has an invalid hire date
-        if ((LocalDate.now().getYear() - dOR.getYear()) > 80){
-            throw new IllegalArgumentException( dOR.toString() + " as a hire date would mean Anita started working over 80 years ago");
-        }
-        // throw error if age over 100
-        if (getAgeInYears() > 100){
-            throw new IllegalArgumentException("Please check the year entered, instructor cannot be over 100 years old");
-        }
     }
     
     // Setters
-    // add courses that instructor can teach
     public void addCourseToInstructorAbilities(String courseCode) {
-        if (!subjectsToTeach.contains(courseCode)){
-            this.subjectsToTeach.add(courseCode);
-        }
+        this.subjectsToTeach.add(courseCode);
     }
     
-    // add courses that instructor can teach
     public void addCourseToAbilities(String subject) {
         subjectsToTeach.add(subject);
     }
     
-    // sets address
     public void changeAddress(String address, String city, String zipCode){
         this.address = address;
         this.city = city;
         this.zipCode = zipCode;
     }
     
-    // set birth day of instructor
     public void setBirthday(LocalDate dOB){
         dateOfBirth = dOB;
     }
     
     // Getters
-    // get age of instructor
     public int getAgeInYears() {
         int age = LocalDate.now().getYear() - dateOfBirth.getYear();
-        if (LocalDate.now().getMonthValue() < dateOfBirth.getMonthValue()){
-            age--;
-        } else if (LocalDate.now().getMonthValue() == dateOfBirth.getMonthValue()){
-            if (LocalDate.now().getDayOfMonth() < dateOfBirth.getDayOfMonth()) {
-                age--;
-            }
-        }
         return age;
     }
     
-    // gets working years at college
     public int NoOfYearsAtCollege() {
         int workingYearsAtCollege = LocalDate.now().getYear() - dateOfRegistration.getYear();
         return workingYearsAtCollege;
     }
     
-    // gets address of instructor
     public String getInstructorAddress(){
-        return address + ", " + city + ", " + zipCode;
+        return address;
     }
     
-    // return list of subjects that instructor is certified to teach
-    public String listOfSubjectsCertifiedToTeach() {
-        String str = "[";
-        if (subjectsToTeach.size() > 0 ){
-            for (int i = 0; i < subjectsToTeach.size(); i++){
-                str += subjectsToTeach.get(i);
-                if (i + 1 < subjectsToTeach.size()) {
-                    str += ", ";
-                } else {
-                    str += "]";
-                }
-            }
-        } else {
-            str = "not qualified to teach courses yet.";
-        }
-        return str;
+    public List<String> listOfSubjectsCertifiedToTeach() {
+        return subjectsToTeach;
     }
     
-    // checks if instructor can teach the subject
     public boolean InstructorcanTeach(String subject){
         return subjectsToTeach.contains(subject);
-    }
-    
-    // gets first name
-    public String getFirstName(){
-        return firstName;
-    }
-    
-    // gets last name
-    public String getLastName(){
-        return lastName;
-    }
-    
-    // return full name
-    public String getFullName(){
-        String fullName = this.getFirstName() + " " + this.getLastName();
-        return fullName;
-    }
-    
-    // return full name
-    public String toString(){
-        return getFullName();
     }
 }
--- NetBeansProjects/Registration/src/main/java/registrationsystem/Student.java
+++ NetBeansProjects/Registration/src/main/java/registrationsystem/Student.java
@@ -6,25 +6,17 @@
 package registrationsystem;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
- * @author Yu-Ming(Terry) Gu
- * @StudentNumber 200410610
- * @Description: Represents a student enrolled in the school
+ * @author Last_
  */
 public class Student {
-    // variables
     String firstName, lastName, address, city, zipCode, program;
     int studentNumber;
     LocalDate dateOfBirth, dateOfEnrollment;
     boolean isSuspended = false;
-    private List<Course> completedCourses = new ArrayList<Course>();
-    private List<Integer> grades = new ArrayList<Integer>();
     
-    // constructor
     public Student (String fName, String lName, String address, String city, String zCode, String program, int stuNumber, LocalDate dOE, LocalDate dOB){
         firstName = fName;
         lastName = lName;
@@ -34,123 +26,43 @@ public class Student {
         this.program = program;
         studentNumber = stuNumber;
         dateOfBirth = dOB;
-        
-        // checks student's age is below 100
-        if (getStudentAge() > 100) {
-            throw new IllegalArgumentException("Please check the year entered, student cannot be over 100 years old");
-        }
         dateOfEnrollment = dOE;
     }
     
     // Setters
-    public void setBirthday(LocalDate dOB) {
-        dateOfBirth = dOB;
-    }
-    
-    // Resets address
     public void changeAddress(String address, String city, String zCode){
         this.address = address;
         this.city = city;
         zipCode = zCode;
     }
     
-    // set isSuspend to true
     public void suspendStudent(){
         isSuspended = true;
     }
     
-    // set isSuspend to false
     public void reinstateStudent(){
         isSuspended = false;
     }
     
-    // add course and grade to list of completed courses
-    public void addCompletedCourse(Course course, int grade){
-        if (grade <= 100 && grade >=0) {
-            completedCourses.add(course);
-            grades.add(grade);
-        } else {
-            throw new IllegalArgumentException("grade must be 0-100 inclusive");
-        }
-    }
-    
     // Getters
-    // gets student number
-    public int getStudentNumber(){
-        return studentNumber;
-    }
-    
-    // gets list of completed courses
-    public String getCoursesCompleted(){
-        String str = "[";
-        for (int i = 0; i < completedCourses.size(); i++){
-            str += completedCourses.get(i).toString() + " grade=" + grades.get(i);
-            if (i + 1 < completedCourses.size()){
-                str += ", ";
-            } else {
-                str += "]";
-            }
-        }
-        return str;
-    }
-    
-    // get student's age
     public int getStudentAge() {
         int age = LocalDate.now().getYear() - dateOfBirth.getYear();
-        if (LocalDate.now().getMonthValue() < dateOfBirth.getMonthValue()){
-            age--;
-        } else if (LocalDate.now().getMonthValue() == dateOfBirth.getMonthValue()){
-            if (LocalDate.now().getDayOfMonth() < dateOfBirth.getDayOfMonth()) {
-                age--;
-            }
-        }
         return age;
     }
     
-    // gets student's address
     public String getStudentAddress(){
-        return address + " " + city + " " + zipCode;
+        return address;
     }
     
-    // gets student's birth day
     public LocalDate getStudentBirthday(){
         return dateOfBirth;
     }
     
-    // gets the enrolled year
-    public int getNoOfYearEnrolled(){
+    public double getNoOfYearEnrolled(){
         return dateOfEnrollment.getYear();
     }
     
-    // checks if students in good standing
     public boolean StudentInGoodStanding(){
         return !isSuspended;
-    }
-    
-    // checks if student has completed the course
-    public boolean hasCompleted(String courseCode){
-        boolean boo = false;
-        for (int i = 0; i < completedCourses.size(); i++){
-            if (completedCourses.get(i).getCourseCode().equals(courseCode) && grades.get(i) > 50) {
-                boo = true;
-            }
-        }
-        return boo;
-    }
-    
-    // gets first name
-    public String getFirstName(){
-        return firstName;
-    }
-    
-    // gets last name
-    public String getLastName(){
-        return lastName;
-    }
-    
-    // return a string
-    public String toString(){
-        String str = getFirstName() + " " + getLastName() + ", student number: " + getStudentNumber();
-        return str;
     }
 }
